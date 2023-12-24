acf343ca816420f0ee37a38a85c786515b68a5d5
Durgesh Tawar <adityatawar58@gmail.com>
2023-12-25 01:31:11 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -7,7 +7,34 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection failed", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
